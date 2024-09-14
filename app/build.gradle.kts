0be9d2b3e79c17b7792753dc9fe32fc1ef4105c7
Saugat Shahi Thakuri <devops.shahi@gmail.com>
2024-09-14 23:45:42 +0545

    :Final Push For Today

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
